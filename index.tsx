c611c12641c5e99cb43b4380dc0ffa2d178113d0
Mike Bannister <notimpossiblemike@gmail.com>
2018-12-08 15:49:08 -0500

    Cleanup

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 const minimist = require('minimist')
+const args: any = minimist(process.argv.slice(2))
 
-const moof = (moof: string) => {}
-
-console.dir(minimist(process.argv.slice(2)))
+console.dir(args)
